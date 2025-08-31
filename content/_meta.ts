2eb0b63daa3675a982e03fd8ea7b0b85729cfd1c
Devashish <dev641science@gmail.com>
2025-08-31 15:37:35 +0530

    Remove dummy Code from the project

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,3 +1,15 @@
 export default {
-  index: { title: "Home" },
+  index: {
+    title: "Home",
+    theme: {
+      typesetting: "article",
+      //   type: "page",
+    },
+  },
+  _templates: {
+    display: "hidden",
+    theme: {
+      pagination: false, // hides from prev/next
+    },
+  },
 };
--- content/blog/_meta.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  index: { title: "blog" },
-  next: { title: "next" },
-};
