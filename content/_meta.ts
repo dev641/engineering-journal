214fd0615d7d10f776de73a56523f383cc206245
Devashish <dev641science@gmail.com>
2025-10-06 15:47:49 +0530

    Refactor meta structure and add new Java meta files

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,8 +1,13 @@
 export default {
-  index: {
+  "index": {
     title: "Home",
     theme: {
       typesetting: "article",
     },
   },
+  "###": {
+    type: "separator",
+    // title: "My Items", // Title is optional
+  },
+  "java": { title: "Java", theme: { typesetting: "article" } },
 };
---
+++ docs/tech-notes/java/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  "concrete-class": {
+    title: "Concrete Class",
+    theme: { typesetting: "article" },
+  },
+};
